179023f5fc20694de8fb968ed71381f47d971382
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-15 16:54:22 -0500

    starting java srcipt

---
+++ script.js
@@ -0,0 +1,32 @@
+var name = 'Name: PELATIAH MORGAN';
+console.log (name);
+
+var careerInfo = 'Career: Socially Awkwrd / Creative Director';
+console.log (careerInfo);
+
+var desc = 'Desrciption: turtles!';
+console.log (desc);
+
+var mi = 'My Interest:';
+console.log (mi);
+
+var mii = 'BEARS';
+console.log (mii);
+var mii= 'BEETS!';
+console.log (mii);
+var mii = 'BATTLE STAR GALLACTICA!';
+console.log (mii);
+
+var pe = 'My Previous Experience:';
+console.log (pe);
+console.log ('*not javascript');
+console.log ('-not javascript');
+console.log ('*not javascript');
+console.log ('-not javascript');
+
+var ms = 'My Skills:';
+console.log (pe);
+console.log ('*sleeping');
+console.log ('*splash');
+console.log ('*rest');
+console.log ('*bite (2 pionts of damage)');
